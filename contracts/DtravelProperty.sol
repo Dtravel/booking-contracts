af5e73778d1af7ae3ea12361c0f4b781feed0a15
danny-dtravel <danny@dtravel.com>
2022-02-04 07:33:32 -0500

    fix book and add cancel

--- contracts/DtravelProperty.sol
+++ contracts/DtravelProperty.sol
@@ -7,9 +7,10 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./DtravelConfig.sol";
 
 struct Booking {
+  uint256 id;
   address customer;
-  uint256 checkInDate;
-  uint256 checkOutDate;
+  uint256 checkInTimestamp;
+  uint256 checkOutTimestamp;
   uint8 status; // 0: booked, 1: fulfilled, 2: cancelled
 }
 
@@ -17,14 +18,14 @@ contract DtravelProperty is Ownable {
   uint256 public id; // property id
   uint256 public price; // property price
   uint256 public cancelPeriod; // cancellation period
-  uint8 public status; // 0: open, 1: filled
-  Booking[] public bookings;
+  Booking[] public bookings; // bookings array
+  mapping(uint256 => bool) filled; // timestamp => bool
+  mapping(uint256 => uint8) bookingStatus; // booking id => 0, 1, 2
 
   constructor(uint256 _id, uint256 _price, uint256 _cancelPeriod) {
     id = _id;
     price = _price;
     cancelPeriod = _cancelPeriod;
-    status = 0;
   }
 
   function updatePrice(uint256 _price) onlyOwner external {
@@ -34,20 +35,55 @@ contract DtravelProperty is Ownable {
 
   function updateCancelPeriod(uint256 _cancelPeriod) onlyOwner external {
     require(_cancelPeriod > 0, "Cancel Period must be over 0");
-    price = _cancelPeriod;
+    cancelPeriod = _cancelPeriod;
   }
 
-  function book(address _token) external {
+  function propertyAvailable(uint256 _checkInTimestamp, uint256 _checkOutTimestamp ) view public returns(bool) {
+    uint256 time = _checkInTimestamp;
+    while (time < _checkOutTimestamp) {
+      if (filled[time] == true)
+        return false;
+    }
+    return true;
+  }
+
+  function book(address _token, uint256 _checkInTimestamp, uint256 _checkOutTimestamp) external {
+    require(_checkInTimestamp > block.timestamp, "Booking for past date is not allowed");
+    require(_checkOutTimestamp > block.timestamp, "Booking for past date is not allowed");
     require(
           IERC20(_token).allowance(owner(), address(this)) >= price,
           "Token allowance too low"
       );
-    require(status == 0, "Property is not available");
+    bool isPropertyAvailable = propertyAvailable(_checkInTimestamp, _checkOutTimestamp);
+    require(isPropertyAvailable == true, "Property is not available");
     bool isSuccess = _safeTransferFrom(IERC20(_token), msg.sender, address(this), price);
     require(isSuccess == true, "Payment failed");
     
-    status = 1;
-    bookings.push(Booking(msg.sender, block.timestamp, 0, 0));
+    uint256 bookingId = bookings.length;
+    uint256 time = _checkInTimestamp;
+    while (time < _checkOutTimestamp) {
+      filled[time] = true;
+      time += 60 * 60 * 24 * 1000;
+    }
+    bookingStatus[bookingId] = 0;
+    bookings.push(Booking(bookingId, msg.sender, _checkInTimestamp, _checkOutTimestamp, 0));
+  }
+
+  function cancel(uint256 _bookingId, uint8 _cancelType) external {
+    require(_bookingId <= bookings.length, "Booking not found");
+    require(bookingStatus[_bookingId] == 0, "Booking is already cancelled");
+    bookingStatus[_bookingId] = _cancelType;
+    bookings[_bookingId].status = _cancelType;
+    Booking memory booking = bookings[_bookingId];
+    
+    uint256 time = booking.checkInTimestamp;
+    uint256 checkOutTimestamp = booking.checkOutTimestamp;
+    while (time < checkOutTimestamp) {
+      filled[time] = false;
+      time += 60 * 60 * 24 * 1000;
+    }
+
+    // Split payment
   }
 
   function bookingHistory() external view returns(Booking[] memory) {
