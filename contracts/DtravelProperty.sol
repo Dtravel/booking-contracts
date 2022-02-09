906270d4d5b1d05de2941e33a1280dd1ceb182fb
danny-dtravel <danny@dtravel.com>
2022-02-09 06:37:28 -0500

    fix comments

--- contracts/DtravelProperty.sol
+++ contracts/DtravelProperty.sol
@@ -8,10 +8,11 @@ import "./DtravelConfig.sol";
 
 struct Booking {
   uint256 id;
-  address customer;
   uint256 checkInTimestamp;
   uint256 checkOutTimestamp;
-  uint8 status; // 0: booked, 1: fulfilled, 2: cancelled
+  uint256 paidAmount;
+  address guest;
+  address token;
 }
 
 contract DtravelProperty is Ownable {
@@ -19,8 +20,8 @@ contract DtravelProperty is Ownable {
   uint256 public price; // property price
   uint256 public cancelPeriod; // cancellation period
   Booking[] public bookings; // bookings array
-  mapping(uint256 => bool) filled; // timestamp => bool
-  mapping(uint256 => uint8) bookingStatus; // booking id => 0, 1, 2
+  mapping(uint256 => bool) public filled; // timestamp => bool
+  mapping(uint256 => uint8) public bookingStatus; // booking id => 0, 1, 2
 
   constructor(uint256 _id, uint256 _price, uint256 _cancelPeriod) {
     id = _id;
@@ -43,47 +44,60 @@ contract DtravelProperty is Ownable {
     while (time < _checkOutTimestamp) {
       if (filled[time] == true)
         return false;
+      time += 60 * 60 * 24;
     }
     return true;
   }
 
-  function book(address _token, uint256 _checkInTimestamp, uint256 _checkOutTimestamp) external {
+  function book(address _token, uint256 _checkInTimestamp, uint256 _checkOutTimestamp) external returns(bool, uint256) {
     require(_checkInTimestamp > block.timestamp, "Booking for past date is not allowed");
-    require(_checkOutTimestamp > block.timestamp, "Booking for past date is not allowed");
-    require(
-          IERC20(_token).allowance(owner(), address(this)) >= price,
-          "Token allowance too low"
-      );
+    require(_checkOutTimestamp > _checkInTimestamp + 60 * 60 * 24, "Booking period should be at least one night");
     bool isPropertyAvailable = propertyAvailable(_checkInTimestamp, _checkOutTimestamp);
     require(isPropertyAvailable == true, "Property is not available");
-    bool isSuccess = _safeTransferFrom(IERC20(_token), msg.sender, address(this), price);
+    uint256 bookingAmount = price * (_checkOutTimestamp - _checkInTimestamp) / (60 * 60 * 24);
+    require(
+          IERC20(_token).allowance(msg.sender, address(this)) >= bookingAmount,
+          "Token allowance too low"
+      );
+    bool isSuccess = _safeTransferFrom(IERC20(_token), msg.sender, address(this), bookingAmount);
     require(isSuccess == true, "Payment failed");
     
     uint256 bookingId = bookings.length;
     uint256 time = _checkInTimestamp;
     while (time < _checkOutTimestamp) {
       filled[time] = true;
-      time += 60 * 60 * 24 * 1000;
+      time += 60 * 60 * 24;
     }
     bookingStatus[bookingId] = 0;
-    bookings.push(Booking(bookingId, msg.sender, _checkInTimestamp, _checkOutTimestamp, 0));
+    bookings.push(Booking(bookingId, _checkInTimestamp, _checkOutTimestamp, bookingAmount, msg.sender, _token));
+
+    return (isSuccess, bookingAmount);
   }
 
-  function cancel(uint256 _bookingId, uint8 _cancelType) external {
+  function cancel(uint256 _bookingId, uint8 _cancelType) external returns(bool) {
     require(_bookingId <= bookings.length, "Booking not found");
-    require(bookingStatus[_bookingId] == 0, "Booking is already cancelled");
-    bookingStatus[_bookingId] = _cancelType;
-    bookings[_bookingId].status = _cancelType;
+    require(bookingStatus[_bookingId] == 0, "Booking is already cancelled or fulfilled");
     Booking memory booking = bookings[_bookingId];
+    require(block.timestamp < booking.checkInTimestamp + cancelPeriod, "Booking has already expired the cancellation period");
+    require(msg.sender == owner() || msg.sender == booking.guest, "You are not authorized to cancel this booking");
     
+    bookingStatus[_bookingId] = _cancelType;
+
+    address guest = booking.guest;
+    uint256 paidAmount = booking.paidAmount;
     uint256 time = booking.checkInTimestamp;
     uint256 checkOutTimestamp = booking.checkOutTimestamp;
     while (time < checkOutTimestamp) {
       filled[time] = false;
-      time += 60 * 60 * 24 * 1000;
+      time += 60 * 60 * 24;
     }
 
-    // Split payment
+    // Refund to the guest
+
+    bool isSuccess = _safeTransferFrom(IERC20(_token), address(this), guest, paidAmount);
+    require(isSuccess == true, "Refund failed");
+
+    return (isSuccess);
   }
 
   function bookingHistory() external view returns(Booking[] memory) {
