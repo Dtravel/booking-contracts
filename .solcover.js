a1b17304b0ead752823ad89678d33221fdd25c8d
danny-dtravel <danny@dtravel.com>
2022-03-18 09:37:54 -0400

    add hardhat environment

---
+++ .solcover.js
@@ -0,0 +1,10 @@
+const skipFiles = ['']
+
+module.exports = {
+  providerOptions: {
+    mnemonic: 'brisk light mimic number pretty surge unknown shiver boy video angry green',
+    network_id: 1337,
+  },
+  skipFiles,
+  istanbulFolder: './reports/coverage',
+}
