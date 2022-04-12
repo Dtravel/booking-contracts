77ad5dce5ac292b42656b625d9285076e898b49d
danny-dtravel <danny@dtravel.com>
2022-04-12 08:41:52 -0400

    add signature verify method

--- contracts/DtravelFactory.sol
+++ contracts/DtravelFactory.sol
@@ -6,18 +6,33 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./DtravelProperty.sol";
 
 contract DtravelFactory is Ownable {
-    DtravelProperty[] private properties;
+    address public configContract;
+    mapping(address => bool) private properties;
 
     event PropertyCreated(uint256 _id, address _property);
+    event Book(uint256 bookingId, uint256 bookedTimestamp);
+    event Cancel(uint256 bookingId);
+    event Payout(uint256 bookingId);
 
-    function deployProperty(
-        uint256 _id,
-        address _config,
-        address _host,
-        address _eventHandler
-    ) public onlyOwner {
-        DtravelProperty property = new DtravelProperty(_id, _config, _host, _eventHandler);
-        properties.push(property);
+    constructor(address _config) {
+        configContract = _config;
+    }
+
+    function deployProperty(uint256 _id, address _host) public onlyOwner {
+        DtravelProperty property = new DtravelProperty(_id, configContract, address(this), _host);
+        properties[address(property)] = true;
         emit PropertyCreated(_id, address(property));
+    }
+
+    function book(uint256 bookingId) external {
+        emit Book(bookingId, block.timestamp);
+    }
+
+    function cancel(uint256 bookingId) external {
+        emit Cancel(bookingId);
+    }
+
+    function payout(uint256 bookingId) external {
+        emit Payout(bookingId);
     }
 }
