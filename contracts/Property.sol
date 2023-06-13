f5481c85a967019998d820f82a0435cbfb0a7684
jacky-dtravel <jacky@dtravel.com>
2023-06-13 10:11:09 +0700

    optimze storing insurance fee

--- contracts/Property.sol
+++ contracts/Property.sol
@@ -160,7 +160,9 @@ contract Property is IProperty, OwnableUpgradeable, ReentrancyGuardUpgradeable {
                 .referralFeeNumerator();
         }
         bookingInfo.status = BookingStatus.IN_PROGRESS;
-        insuranceInfo[_setting.bookingId] = _setting.insuranceInfo;
+        if (_setting.insuranceInfo.damageProtectionFee > 0) {
+            insuranceInfo[_setting.bookingId] = _setting.insuranceInfo;
+        }
 
         uint256 n = _setting.policies.length;
         for (uint256 i; i < n; i++)
