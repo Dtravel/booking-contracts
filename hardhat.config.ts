cf8143d630a8160e89bdb6cc3ecd1d85ac30f56d
jacky-dtravel <jacky@dtravel.com>
2023-08-28 17:05:01 +0700

    chore: correct project config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,72 +11,6 @@ import "./hardhat.tasks";
 
 dotenv.config();
 
-import '@nomiclabs/hardhat-ethers'
-import '@nomiclabs/hardhat-etherscan'
-import '@nomiclabs/hardhat-waffle'
-import 'hardhat-abi-exporter'
-import 'hardhat-gas-reporter'
-import 'hardhat-contract-sizer'
-import '@tenderly/hardhat-tenderly'
-import '@openzeppelin/hardhat-upgrades'
-import '@typechain/hardhat'
-
-// Networks
-
-interface NetworkConfig {
-  network: string
-  chainId: number
-  gas?: number | 'auto'
-  gasPrice?: number | 'auto'
-}
-
-const networkConfigs: NetworkConfig[] = [
-  { network: 'mainnet', chainId: 1 },
-  { network: 'ropsten', chainId: 3 },
-  { network: 'rinkeby', chainId: 4 },
-  { network: 'kovan', chainId: 42 },
-  { network: 'bscmainnet', chainId: 56 },
-  { network: 'bsctestnet', chainId: 97 },
-]
-
-function getAccountMnemonic() {
-  return process.env.MNEMONIC || ''
-}
-
-function getAccountPrivateKey() {
-  return process.env.PRIVATE_KEY || ''
-}
-
-function getDefaultProviderURL(network: string) {
-  if (network == 'bscmainnet') {
-    return 'https://bsc-dataseed.binance.org/'
-  } else if (network == 'bsctestnet') {
-    return 'https://data-seed-prebsc-1-s1.binance.org:8545/'
-  }
-  return `https://${network}.infura.io/v3/${process.env.INFURA_KEY}`
-}
-
-function setupDefaultNetworkProviders(buidlerConfig) {
-  for (const netConfig of networkConfigs) {
-    buidlerConfig.networks[netConfig.network] = {
-      chainId: netConfig.chainId,
-      url: getDefaultProviderURL(netConfig.network),
-      gas: netConfig.gasPrice || 'auto',
-      gasPrice: netConfig.gasPrice || 'auto',
-      accounts: {
-        mnemonic: getAccountMnemonic(),
-      },
-    }
-  }
-}
-
-// Tasks
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.9",
@@ -116,6 +50,4 @@ const config: HardhatUserConfig = {
   },
 };
 
-setupDefaultNetworkProviders(config)
-
-export default config
+export default config;
