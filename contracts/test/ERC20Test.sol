58becac74ce56489671ee958122ab6d263fe9fed
jacky-dtravel <jacky@dtravel.com>
2023-08-28 17:04:22 +0700

    chore: format codes

--- contracts/test/ERC20Test.sol
+++ contracts/test/ERC20Test.sol
@@ -5,11 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Test is ERC20 {
     constructor(string memory _name, string memory _symbol)
-        ERC20(_name, _symbol)
-    // solhint-disable-next-line
-    {
-
-    }
+        ERC20(_name, _symbol) // solhint-disable-next-line
+    {}
 
     function mint(address _to, uint256 _amount) external {
         _mint(_to, _amount);
